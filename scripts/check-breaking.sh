f885657f95e3c614b6f454f6803773b4675a13e3
Don Johnson <don@codetestcode.io>
2025-05-20 19:33:26 -0700

    Properly version API changes with deprecation notice

--- scripts/check-breaking.sh
+++ scripts/check-breaking.sh
@@ -5,4 +5,4 @@ BASE=api/v1/openapi.yaml
 REV=api/v2/openapi.yaml
 
 echo "Checking breaking changes..."
-oasdiff breaking "$BASE" "$REV" --fail-on ERR --format text
+oasdiff breaking "$BASE" "$REV" --fail-on WARN --format text
