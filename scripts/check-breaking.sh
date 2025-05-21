1fb3e67598ea71131af0c2e1ad168a7d5f3bb054
Don Johnson <don@codetestcode.io>
2025-05-20 19:17:41 -0700

    Initial commit

---
+++ scripts/check-breaking.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+BASE=api/v1/openapi.yaml
+REV=api/v2/openapi.yaml
+
+echo "Checking breaking changes..."
+oasdiff breaking "$BASE" "$REV" --fail-on ERR --format text --resolve-refs
